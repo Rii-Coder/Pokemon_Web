fbcac0eb3b9f0c491f6b583b19a4d44e79e104e3
TheR2D2cx <63327126+TheR2D2cx@users.noreply.github.com>
2021-10-26 21:44:45 -0700

    Delete terminado

--- Servicio RESTful/servicio.js
+++ Servicio RESTful/servicio.js
@@ -1,12 +1,22 @@
 const express = require('express')
+const winston = require("winston");
 const app = express();
 app.use(express.json());
 const bodyParse = require('body-parser');
 app.use(express.json());
 app.use(express.static("./public"));
-const {MongoClient} = require('mongodb');
+const {MongoClient, Logger} = require('mongodb');
 const url = 'mongodb://localhost/pokemon';
 const client = new MongoClient(url);
+const logger = winston.createLogger({
+    level: "warn",
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({
+            filename: "logs.txt"
+        })
+    ]
+});
 
 let entrenador ={  
   "_id": 3,
@@ -31,7 +41,32 @@ let entrenador ={
     
   }
   
-  }
+}
+
+let entrenador2 = {
+    "_id": 4,
+    "nombre": "Pepe",
+    "email": "pepe@gmail.com",
+    "password": "123456",
+    "caja": {
+        "_id": 3,
+        "pokemones": [
+            12,
+            22,
+            32,
+            42
+        ]
+    },
+    "equipo": {
+        "_id": 3,
+        "pokemones": [
+            12,
+            42
+        ]
+
+    }
+
+}
 
 
 
@@ -40,21 +75,31 @@ let entrenador ={
 app.get("/obtenerEntrenador:id", (req, res) => {
   
     let id = req.params.id;
-    entranador.id = id;
-    var dummy = JSON.stringify(entranador)
+    entrenador.id = id;
+    var dummy = JSON.stringify(entrenador)
     let entre = dummy
     res.status(201).json(entre);
   });
   
-  app.post("/insertarEntrenador", (req, res) => {
+app.post("/insertarEntrenador", (req, res) => {
+    client.connect();
       function insertar(client, newEntrenador){
-      client.connect();
       const result = client.db("pokemon").collection("entrenador").insertOne(newEntrenador);
       return result;
     }
-    insertar(client, entrenador)
+      insertar(client, entrenador)
     res.status(201).json(entrenador);
-  });
+});
+
+app.post("/insertarEntrenador2", (req, res) => {
+    client.connect();
+    function insertar(client, newEntrenador) {
+        const result = client.db("pokemon").collection("entrenador").insertOne(newEntrenador);
+        return result;
+    }
+    insertar(client, entrenador2)
+    res.status(201).json(entrenador2);
+});
   
   app.put("/actualizarEntrenador:id", (req, res) => {
     let id = req.params.id;
@@ -64,8 +109,21 @@ app.get("/obtenerEntrenador:id", (req, res) => {
     res.status(201).json(entre);
   });
   
-  app.delete("/eliminarEntrenador:id", (req, res) => {
-    res.send(`Eliminado ${req.params.id}`);
+app.delete("/eliminarEntrenador:id", (req, res) => {
+    client.connect();
+    function eliminar(client, id) {
+        const idnew = parseInt(+""+req.params.id);
+        const result = client.db("pokemon").collection("entrenador").deleteOne({ _id: idnew });
+        return result;
+    }
+    try {
+        eliminar(client, req.params.id);
+        res.send(`Eliminado ${req.params.id}`);
+    } catch (error) {
+        res.send("Problemas para eliminar");
+    }
+    
+    
   });
 
   
